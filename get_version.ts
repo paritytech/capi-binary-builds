686afeef7825b4ac1ba94b1569730d02b729ca23
tjjfvi <t6@t6.fyi>
2023-03-23 08:50:35 -0400

    separate version and ref

--- get_version.ts
+++ get_version.ts
@@ -1,13 +1,13 @@
 const [type, repo] = Deno.args
 
-const types: Record<string, () => Promise<string>> = {
+const types: Record<string, () => Promise<[string, string?]>> = {
   async release() {
     const release = await fetch(`https://api.github.com/repos/${repo}/releases/latest`)
       .then((r) => r.json())
 
     console.log(release)
 
-    return release.tag_name
+    return [release.tag_name]
   },
   async commit() {
     const commit = await fetch(`https://api.github.com/repos/${repo}/commits/@`)
@@ -15,14 +15,15 @@ const types: Record<string, () => Promise<string>> = {
 
     console.log(commit)
 
-    return commit.sha
+    return [commit.sha.slice(0, 7), commit.sha]
   },
 }
 
 if (!(type in types)) throw new Error(`unrecognized type ${type}`)
 
-const version = await types[type]()
+const [version, ref = version] = await types[type]()
 
 if (!version) throw new Error("version fetching failed")
 
 await Deno.writeTextFile(Deno.env.get("GITHUB_OUTPUT")!, `version=${version}`, { append: true })
+await Deno.writeTextFile(Deno.env.get("GITHUB_OUTPUT")!, `ref=${ref}`, { append: true })
