c57e6b3427ae46cbb25969f04e7b74e55c12212e
T6 <t6@t6.fyi>
2023-03-23 18:50:35 -0400

    feat: add `trappist-collator` and `stout-collator` (#4)

--- get_version.ts
+++ get_version.ts
@@ -1,7 +1,35 @@
-const [repo] = Deno.args
+const [type, repo] = Deno.args
 
-const version = await fetch(`https://api.github.com/repos/${repo}/releases/latest`)
-  .then((r) => r.json())
-  .then((r) => r.tag_name)
+const types: Record<string, () => Promise<[string, string?]>> = {
+  async release() {
+    const release = await fetch(`https://api.github.com/repos/${repo}/releases/latest`)
+      .then((r) => r.json())
 
-await Deno.writeTextFile(Deno.env.get("GITHUB_OUTPUT")!, `version=${version}`, { append: true })
+    console.log(release)
+
+    return [release.tag_name]
+  },
+  async commit() {
+    const commit = await fetch(`https://api.github.com/repos/${repo}/commits/@`)
+      .then((r) => r.json())
+
+    console.log(commit)
+
+    return [commit.sha.slice(0, 7), commit.sha]
+  },
+}
+
+if (!(type in types)) throw new Error(`unrecognized type ${type}`)
+
+const [version, ref = version] = await types[type]()
+
+if (!version) throw new Error("version fetching failed")
+
+await Deno.writeTextFile(
+  Deno.env.get("GITHUB_OUTPUT")!,
+  `
+version=${version}
+ref=${ref}
+`.trim(),
+  { append: true },
+)
